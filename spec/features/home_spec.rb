297cb31cde59723d6a11da2ace95e7297760a674
Mikhail Skliar <skliarmikhail@gmail.com>
2023-09-08 10:32:00 +0300

    Add feature test for language switching

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -22,33 +22,5 @@ RSpec.describe 'Homes' do
 
       it { expect(page).to have_xpath "//ul[contains(@class,'dropdown-menu')]" }
     end
-
-    describe 'switch language to ru' do
-      before do
-        click_link 'Language'
-        click_link 'RU'
-      end
-
-      it { expect(page).to have_link 'Язык' }
-    end
-
-    describe 'switch language to en' do
-      before do
-        click_link 'Language'
-        click_link 'EN'
-      end
-
-      it { expect(page).to have_link 'Language' }
-    end
-
-    describe 'switch language to en after choosing ru' do
-      before do
-        click_link 'Language'
-        click_link 'RU'
-        click_link 'АНГ'
-      end
-
-      it { expect(page).to have_link 'Language' }
-    end
   end
 end
---
+++ spec/features/language_switching_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'LanguageSwitchings' do
+  describe 'language link' do
+    before { visit root_path }
+
+    describe 'switch language to ru' do
+      before do
+        click_link 'Language'
+        click_link 'RU'
+      end
+
+      it { expect(page).to have_link 'Язык' }
+    end
+
+    describe 'switch language to en' do
+      before do
+        click_link 'Language'
+        click_link 'EN'
+      end
+
+      it { expect(page).to have_link 'Language' }
+    end
+
+    describe 'switch language to en after choosing ru' do
+      before do
+        click_link 'Language'
+        click_link 'RU'
+        click_link 'АНГ'
+      end
+
+      it { expect(page).to have_link 'Language' }
+    end
+  end
+end
