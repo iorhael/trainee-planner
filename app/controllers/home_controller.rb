032724776c155b69627fa88b4c1520a53507744b
Mikhail Skliar <skliarmikhail@gmail.com>
2023-09-22 10:16:59 +0300

    Update information on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @events_counter = EventsCounterService.new(current_user) if current_user
+  end
 end
