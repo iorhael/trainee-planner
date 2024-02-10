93d5c36b8bf604d6c5a176cf787118dd626f4b41
Mikhail Skliar <132287268+iorhael@users.noreply.github.com>
2024-02-10 10:35:05 +0300

    Configure redis for caching (#49)
    * Add redis
    
    * Add redis as cache_store
    
    * Update sidekiq configuration

--- Gemfile
+++ Gemfile
@@ -29,8 +29,8 @@ gem 'stimulus-rails'
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem 'jbuilder'
 
-# Use Redis adapter to run Action Cable in production
-# gem "redis", "~> 4.0"
+# A Ruby client library for Redis [https://github.com/redis/redis-rb]
+gem 'redis'
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@
 require 'sidekiq/scheduler'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6380') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'), db: ENV.fetch('SIDEKIQ_DB', 1) }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6380') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'), db: ENV.fetch('SIDEKIQ_DB', 1) }
 end
