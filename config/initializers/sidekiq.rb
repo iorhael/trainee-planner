1e0761bf22abecc350412a6b77817f0ba9972eff
Mikhail Skliar <skliarmikhail@gmail.com>
2024-02-11 23:43:53 +0300

    Update sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@
 require 'sidekiq/scheduler'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'), db: ENV.fetch('SIDEKIQ_DB', 1) }
+  config.redis = { host: ENV.fetch('REDIS_HOST', 'localhost'), db: ENV.fetch('SIDEKIQ_DB', 1) }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379'), db: ENV.fetch('SIDEKIQ_DB', 1) }
+  config.redis = { host: ENV.fetch('REDIS_HOST', 'localhost'), db: ENV.fetch('SIDEKIQ_DB', 1) }
 end
